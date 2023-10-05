7a645f4940ee3dfffffa72d93b2ad17685284fc9
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2023-10-05 14:28:38 +0100

    add withdraw fund me script

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -22,8 +22,27 @@ contract FundFundMe is Script {
             block.chainid
         );
 
+        vm.startBroadcast();
         fundFundMe(mostRecentlyDeployed);
+        vm.stopBroadcast();
     }
 }
 
-contract WithdrawFundMe is Script {}
+contract WithdrawFundMe is Script {
+    function withdrawFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).withdraw();
+        vm.stopBroadcast();
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+
+        vm.startBroadcast();
+        withdrawFundMe(mostRecentlyDeployed);
+        vm.stopBroadcast();
+    }
+}
